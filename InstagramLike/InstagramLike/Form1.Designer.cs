b66060c08461c3a3cf220c966349388cdb701a07
Musa Köşker <musa5271@gmail.com>
2018-05-31 18:31:30 +0300

    Tasarım
    Tasarım Bitti, WebBrowser Eklendi

---
+++ InstagramLike/InstagramLike/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace InstagramLike
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bunifuDragControl1 = new ns1.BunifuDragControl(this.components);
+            this.bunifuElipse1 = new ns1.BunifuElipse(this.components);
+            this.bunifuGradientPanel1 = new ns1.BunifuGradientPanel();
+            this.bunifuWebClient1 = new ns1.BunifuWebClient(this.components);
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.bunifuGradientPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this.bunifuGradientPanel1;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.webBrowser1);
+            this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.Black;
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Pink;
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.Violet;
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.Green;
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(411, 0);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(373, 553);
+            this.bunifuGradientPanel1.TabIndex = 0;
+            // 
+            // bunifuWebClient1
+            // 
+            this.bunifuWebClient1.AllowReadStreamBuffering = false;
+            this.bunifuWebClient1.AllowWriteStreamBuffering = false;
+            this.bunifuWebClient1.BaseAddress = "";
+            this.bunifuWebClient1.CachePolicy = null;
+            this.bunifuWebClient1.Credentials = null;
+            this.bunifuWebClient1.Encoding = ((System.Text.Encoding)(resources.GetObject("bunifuWebClient1.Encoding")));
+            this.bunifuWebClient1.Headers = ((System.Net.WebHeaderCollection)(resources.GetObject("bunifuWebClient1.Headers")));
+            this.bunifuWebClient1.QueryString = ((System.Collections.Specialized.NameValueCollection)(resources.GetObject("bunifuWebClient1.QueryString")));
+            this.bunifuWebClient1.UseDefaultCredentials = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(20, 553);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 553);
+            this.Controls.Add(this.bunifuGradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ns1.BunifuDragControl bunifuDragControl1;
+        private ns1.BunifuElipse bunifuElipse1;
+        private ns1.BunifuGradientPanel bunifuGradientPanel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private ns1.BunifuWebClient bunifuWebClient1;
+
+    }
+}
+
